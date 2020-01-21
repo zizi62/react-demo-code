4da9b4931bcac9f5c925f72b925afcc9fb2e1986
Zoia Reutckaia <zizi2405@gmail.com>
2020-01-21 14:00:44 -0500

    Updates

---
+++ precache-manifest.23c66ce4dcb3375576c039b95cac3041.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19c19195ddca54259d733b748219cdc",
+    "url": "/zizi62/reactDemo.git/index.html"
+  },
+  {
+    "revision": "56738e5c9f59f6fce321",
+    "url": "/zizi62/reactDemo.git/static/css/main.3923d170.chunk.css"
+  },
+  {
+    "revision": "305c6d9c2855f0de48b5",
+    "url": "/zizi62/reactDemo.git/static/js/2.73519b12.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/zizi62/reactDemo.git/static/js/2.73519b12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56738e5c9f59f6fce321",
+    "url": "/zizi62/reactDemo.git/static/js/main.594831e2.chunk.js"
+  },
+  {
+    "revision": "b082267f6f765dfbb16a",
+    "url": "/zizi62/reactDemo.git/static/js/runtime-main.0c58bff3.js"
+  }
+]);
