68938f82a0528680f4e29cf98681a8c588dbe006
Zoia Reutckaia <zizi2405@gmail.com>
2020-01-21 15:25:01 -0500

    hashRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
